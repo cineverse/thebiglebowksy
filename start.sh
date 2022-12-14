dbcd4f32cdd78332512f6d47588a2579fbc17524
cineverse <102945410+cineverse@users.noreply.github.com>
2022-12-14 21:16:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-python3 update.py && python3 -m bot
+python3 update.py
+
+pip3 install yt_dlp
+
+pip3 install youtube_dl
+
+pip3 install qbittorrent-api
+
+pip3 install aria2p
+
+pip3 install speedtest-cli
+
+python3 -m bot
